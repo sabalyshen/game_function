7bd402f753c5384d6a91ad7ffa74b2a7a21301ee
sabalyshen <tedoldhead@gmail.com>
2021-07-26 20:40:46 +0800

    second

--- game.py
+++ game.py
@@ -1,18 +1,14 @@
-#檢查有沒有之前的檔案
+import os # 載入作業系統operating system
+
+#讀取檔案
 def read_file(filename):
-    import os # 載入作業系統operating system
     games = []
-    if os.path.isfile(filename):#檢查檔案在不在
-        print('讀取以前檔案')
-        with open(filename, 'r', encoding = 'utf-8') as f:
-            for line in f:
-                if '遊戲, 價格' in line:
-                    continue # 跳到下一個迴圈
-                name, price = line.strip().split(',') 
-                games.append([name, price])
-        print(games)
-    else:
-        print('歡迎!初次使用')
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '遊戲, 價格' in line:
+                continue # 跳到下一個迴圈
+            name, price = line.strip().split(',') 
+            games.append([name, price])
     return games
 
 #讓使用者輸入
@@ -40,7 +36,20 @@ def write_file(filename, games):
         for g in games:
             f.write(g[0] + ',' + str(g[1]) + '\n') 
 
-games = read_file('games.csv')
-games = user_input(games)
-print_game(games)
-write_file('games.csv', games)
+def main():
+    filename = 'games.csv'
+    if os.path.isfile(filename):#檢查檔案在不在
+        print('讀取以前檔案')
+        games = read_file(filename)
+    else:
+        print('歡迎!初次使用')
+    games = user_input(games)
+    print_game(games)
+    write_file('games.csv', games)
+
+main()
+
+#refactor 重構，程式已function重新製作
+#1.一個function執行一個動作
+#2.有一個main function執行所有動作
+#3.輸入物件不重複出現
